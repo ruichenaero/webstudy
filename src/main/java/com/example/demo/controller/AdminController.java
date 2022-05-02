ecc36ed4522ea8278354123d0814689b821ce659
ruichenaero <chenruiaero@outlook.com>
2022-05-02 23:42:10 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -0,0 +1,11 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author 陈锐
+ */
+@Controller
+public class AdminController extends BaseController{
+
+}
